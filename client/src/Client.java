e2450f712ed6d19a7e825b1c80d32df4fe955a08
Bradon Lodwick <100585662@oncampus.local>
2018-11-06 15:32:10 -0500

    Added base functionality for K-means distribution

---
+++ client/src/Client.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Client {
+    public static void main(String[] args) {
+        ArrayList<Point> centroids = new ArrayList<>();
+        centroids.add(new Point(0, 0));
+        centroids.add(new Point(1, 0));
+        centroids.add(new Point(2, 0));
+
+        Point p1 = new Point(3, 10);
+        Point p2 = new Point(2, 10);
+        Point p3 = new Point(0.25, 10);
+
+        ArrayList<ArrayList<Point>> client1 = new ArrayList<>();
+        ArrayList<ArrayList<Point>> client2 = new ArrayList<>();
+        ArrayList<ArrayList<Point>> client3 = new ArrayList<>();
+
+
+    }
+}
